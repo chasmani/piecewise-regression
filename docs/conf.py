194975c43043ec87f793efe5186800162b92354d
chasmani <pilgrimcharlie2@gmail.com>
2021-09-14 16:53:27 +0100

    Changed read the docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,11 @@ release = '1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+	'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,7 +52,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -58,5 +63,3 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('..'))
-extensions = [
-'sphinx.ext.autodoc',]
